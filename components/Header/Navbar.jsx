1aac58d301ff2fc593063ad1adff86deab797d70
Arman Khan <sparmankhan@gmail.com>
2024-02-03 00:08:25 +0600

    message and notification added

--- components/Header/Navbar.jsx
+++ components/Header/Navbar.jsx
@@ -1,4 +1,7 @@
+import Link from "next/link";
+import { AiOutlineMessage } from "react-icons/ai";
 import { RiMenu4Fill } from "react-icons/ri";
+import { VscBell } from "react-icons/vsc";
 function Navbar() {
     return (
         <nav className="navbar sticky top-0 z-50 bg-base-100">
@@ -14,9 +17,64 @@ function Navbar() {
           <a className="btn btn-ghost text-xl">Logo</a>
         </div>
         <div className="flex-none gap-2">
+{/* Search */}
           <div className="form-control">
-            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+            <input type="search" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
           </div>
+          {/* Notifications */}
+          <div className="dropdown dropdown-end">
+  <div tabIndex={0} role="button" className="">
+  {/* <VscBellDot  /> */}
+    <VscBell size={32} /></div>
+    <ul tabIndex={0} className="dropdown-content border border-zinc-600 z-[1] menu p-2 shadow bg-base-100 rounded-md min-w-44 max-w-80 w-80">
+  {
+    [...Array(6).keys()]?.map((chat,i)=>{
+      return   <li key={i}>
+      <Link href={`#`}>
+      <div className="flex gap-3 truncate"> 
+      <img className="w-12 h-12 rounded-full" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="" />
+      <div className="w-full flex flex-col">
+      <h3 className="flex items-center gap-1"><b>User Name</b><p>Report on hello world</p></h3>
+      <p>Hello world amar sonar bangla ami tomay love kori</p>
+      </div>
+      </div>
+     
+      </Link>
+    </li>
+    })
+  }
+  <Link className="btn btn-success flex items-center rounded text-white" href={`#`}>
+     See All Reports
+      </Link>
+  </ul>
+</div>
+          {/* Messages */}
+          <div className="dropdown dropdown-end ">
+  <div tabIndex={0} role="button">
+  {/* <VscBellDot  /> */}
+    <AiOutlineMessage size={32} /></div>
+  <ul tabIndex={0} className="dropdown-content border border-zinc-600 z-[1] menu p-2 shadow bg-base-100 rounded-md min-w-44 max-w-80 w-80">
+  {
+    [...Array(6).keys()]?.map((chat,i)=>{
+      return   <li key={i}>
+      <Link href={`#`}>
+      <div className="flex gap-3 truncate"> 
+      <img className="w-12 h-12 rounded-full" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="" />
+      <div className="w-full flex flex-col">
+      <b>User Name</b>
+      <p>Hello world amar sonar bangla ami tomay love kori</p>
+      </div>
+      </div>
+     
+      </Link>
+    </li>
+    })
+  }
+  <Link className="btn btn-success flex items-center rounded text-white" href={`#`}>
+     See All Messages
+      </Link>
+  </ul>
+</div>
           <div className="dropdown dropdown-end">
             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
